aed07b3ab805aac3192a12a3bc3df46ec1fb44ac
amadrocky <ak45amad@hotmail.fr>
2021-10-25 22:13:20 +0200

    Shop page

---
+++ src/Controller/ShopController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/boutique", name="shop_")
+ */
+class ShopController extends AbstractController
+{
+
+    /**
+     * @Route("/boutique", name="index")
+     *
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('shop/index.html.twig', [
+            
+        ]);
+    }
+}
