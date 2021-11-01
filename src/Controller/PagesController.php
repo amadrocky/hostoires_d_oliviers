f8af25a2e5696c1abbf34b6919f74c14ccb5fa9a
amadrocky <ak45amad@hotmail.fr>
2021-11-01 17:27:09 +0100

    Adding pages & start advices

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    /**
+     * @Route("/conseils-entretien", name="advices")
+     */
+    public function advicesPage(): Response
+    {
+        return $this->render('pages/advices.html.twig');
+    }
+
+    /**
+     * @Route("/livraison-plantation", name="delivery")
+     */
+    public function deliveryPage(): Response
+    {
+        return $this->render('pages/delivery.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactPage(): Response
+    {
+        return $this->render('pages/contact.html.twig');
+    }
+}
